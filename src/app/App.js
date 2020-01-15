7c0e8b407ad77629a7adc88c60532285313c0354
Assaf Gannon <assaf.gannon@gmai.com>
2020-01-15 11:06:55 +0200

    comments

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,29 @@
-import React, { memo, useState } from 'react';
+import React, { useMemo, useState, useEffect, useCallback } from 'react';
 import Header from '../header/Header';
 import SearchInput from '../searchInput/SearchInput';
 import EmojiResults from '../emojiResults/EmojiResults';
 import filterEmoji from '../filterEmoji';
 
+const filter = filterEmoji(20);
 
 const App = () => {
-  const [filteredEmoji, setFilteredEmoji] = useState(filterEmoji('', 20));
-  const handleSearchChange = (event) => setFilteredEmoji(filterEmoji(event.target.value, 20));
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredEmoji, setFilteredEmoji] = useState([]);
 
-  return (
-    <div>
-      <Header />
-      <SearchInput textChange={handleSearchChange} />
-      <EmojiResults emojiData={filteredEmoji} />
-    </div>
-  );
+    useEffect(() => {
+        setFilteredEmoji(filter(searchQuery));
+    }, [searchQuery]);
+
+    const handleSearchCahanged = useCallback(event => {
+        setSearchQuery(event.target.value);
+    }, []);
+
+    return (
+        <div>
+            <Header />
+            <SearchInput searchQuery={searchQuery} textChange={handleSearchCahanged} />
+            <EmojiResults emojiData={filteredEmoji} />
+        </div>
+    );
 };
-export default memo(App);
+export default App;
--- src/searchInput/SearchInput.js
+++ src/searchInput/SearchInput.js
@@ -2,17 +2,17 @@ import React, { memo } from 'react';
 
 import './SearchInput.css';
 
+const SearchInput = props => {
+    const { textChange, searchQuery } = props;
+    // const handleChange = (event) => textChange(event);
+    console.log('rendering SearchInput', searchQuery);
 
-const SearchInput = (props) => {
-  const { textChange } = props;
-  const handleChange = (event) => textChange(event);
-
-  return (
-    <div className="component-search-input">
-      <div>
-        <input onChange={handleChange} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="component-search-input">
+            <div>
+                <input onChange={textChange} value={searchQuery} />
+            </div>
+        </div>
+    );
 };
 export default memo(SearchInput);
